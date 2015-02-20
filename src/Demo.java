4f9c6dfeba4e098b179b9f1cd18650217ab80030
StarWix <starwix@ya.ru>
2015-02-20 15:28:03 +0300

    Add mouse click when cursor arrives at the target. Rename class Cursor to Mouse.

--- src/Demo.java
+++ src/Demo.java
@@ -1,15 +1,16 @@
 import java.awt.AWTException;
 import java.awt.Point;
 
-import net.starwix.Cursor;
+import net.starwix.Mouse;
 
 
 public class Demo {
 	
 	public static void main(String[] args) {
 		try {
-			Cursor cursor = new Cursor(5000);
-			cursor.move(new Point(500, 250));
+			Mouse mouse = new Mouse(100);
+			mouse.move(new Point(500, 250), true);
+			mouse.move(new Point(1024, 0));
 		} catch (AWTException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
